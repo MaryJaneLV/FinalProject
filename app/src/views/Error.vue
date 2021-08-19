65e162c823a409ba5a3195995c826b5db30cc5e9
Maria Jakovleva <mariajakovleva@Marias-MacBook-Air.local>
2021-08-19 16:19:43 +0300

    first commit

---
+++ app/src/views/Error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="error">
+    <h1>404</h1>
+    <h4>Page not found</h4>
+  </div>
+</template>
+
+<style>
+.error {
+  color: darkblue;
+  font-size: 60px;
+  padding: 100px;
+  display: inline-block;
+}
+
+h1,
+h4 {
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+</style>
---
+++ app/src/views/Posts.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="content">
+    <h1>All posts go here</h1>
+  </div>
+</template>
+
+<style>
+.content {
+  padding: 100px;
+}
+</style>
