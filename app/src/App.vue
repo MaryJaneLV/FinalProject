b813d276ee9f6db406a64c0410ed534737db8161
Olga Rizenkova <rekrutenko.o@gmail.com>
2021-08-25 20:27:14 +0300

    Added MaryJane files

---
+++ app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header />
+    <router-view> </router-view>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Header
+  },
+  methods: {
+    clicker() {}
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: darkblue;
+  position: absolute;
+}
+</style>
---
+++ app/src/views/AllPosts.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">
+    <h1>All posts go here</h1>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.content {
+  position: absolute;
+  text-align: center;
+  width: 100%;
+}
+</style>
